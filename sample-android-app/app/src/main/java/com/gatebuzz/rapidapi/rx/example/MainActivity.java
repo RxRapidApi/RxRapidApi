4303a2edec87db2731bd93e979d0fa206782f601
Paul Hawke <paul.hawke@gmail.com>
2017-01-07 23:38:58 -0600

    polished the demo app

--- sample-android-app/app/src/main/java/com/gatebuzz/rapidapi/rx/example/MainActivity.java
+++ sample-android-app/app/src/main/java/com/gatebuzz/rapidapi/rx/example/MainActivity.java
@@ -2,23 +2,13 @@ package com.gatebuzz.rapidapi.rx.example;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 
-import com.gatebuzz.rapidapi.rx.FailedCallException;
+import com.gatebuzz.rapidapi.rx.example.hackernews.HackerNewsActivity;
+import com.gatebuzz.rapidapi.rx.example.nasa.NasaActivity;
 import com.gatebuzz.rapidapi.rx.example.spotify.search.SpotifySearchActivity;
-import com.gatebuzz.rapidapi.rx.example.zillow.ZillowApi;
 import com.jakewharton.rxbinding.view.RxView;
 
-import java.util.Map;
-
-import rx.Observable;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
-
-@SuppressWarnings("unused")
 public class MainActivity extends ManagedSubscriptionsActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,36 +23,9 @@ public class MainActivity extends ManagedSubscriptionsActivity {
         super.onResume();
         subscriptions.addAll(
                 RxView.clicks(findViewById(R.id.spotify)).subscribe(aVoid -> SpotifySearchActivity.launch(MainActivity.this)),
-                RxView.clicks(findViewById(R.id.fab)).subscribe(x -> tryTheApi())
+                RxView.clicks(findViewById(R.id.nasa)).subscribe(aVoid -> NasaActivity.launch(MainActivity.this)),
+                RxView.clicks(findViewById(R.id.hackernews)).subscribe(aVoid -> HackerNewsActivity.launch(MainActivity.this))
         );
-    }
-
-    private void tryTheApi() {
-        ZillowApi zillowApi = ((ExampleApplication) getApplication()).getZillowApi();
-
-        Observable<Map<String, Object>> foo = zillowApi.getDeepSearchResults("2039 Yale Avenue", "63143", false);
-
-        foo.subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Map<String, Object>>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e("RxRapidApi", "Error calling service", e);
-                        if (e instanceof FailedCallException) {
-                            Log.e("RxRapidApi", "Error response:" + ((FailedCallException) e).getResponse());
-                        }
-                    }
-
-                    @Override
-                    public void onNext(Map<String, Object> result) {
-                        Log.e("RxRapidApi", "Service result:" + result);
-                    }
-                });
     }
 
 }
